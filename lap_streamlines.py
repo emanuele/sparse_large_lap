e0e156eefee138aba5c0b98c440ef9238c348748
Gabriele Amorosino <g.amorosino@gmail.com>
2020-11-20 18:02:28 +0100

    several changes

--- lap_streamlines.py
+++ lap_streamlines.py
@@ -14,12 +14,15 @@ from scipy import sparse
 from time import time 
 from matching import min_weight_full_bipartite_matching
 from scipy.optimize import linear_sum_assignment
+import os
 
 np.random.seed(0)
-N_streamlines1=8000
-N_streamlines2=8000
+N_streamlines1=50000#00
+N_streamlines2=50000#00
 N_points=32
-k=500
+k=1000
+lap_flag=False
+MWFBM_flag=False
 
 # %% Load Trk
 print("laod stramlines...")
@@ -36,6 +39,7 @@ print("setting the same number of points for both the tracts...")
 
 track1=np.array(set_number_of_points( track1 , N_points ))
 track2=np.array(set_number_of_points( track2 ,  N_points ))
+
 # %% Nearest Neightbours
 print("Nearest Neightbours")
 
@@ -55,18 +59,44 @@ cost[tmp, neighbours] = distances
 
 cost1=cost.todense() 
 cost1[cost1==0]=np.inf #10**4
-print('Linear Sum Assigment')
-t0 = time()
-row_ind1, col_ind1 = linear_sum_assignment(cost1)
-print('%s sec' % (time() - t0))
-total_cost1 = cost[row_ind1, col_ind1].sum()
-print('lap cost=%s' % total_cost1)
+
+# %% Test on matlab
+import scipy.io as sio
+data={ 'cost': cost }
+matfile="/home/gamorosino/local/sparse_large_lap/cost_sparse.mat"
+sio.savemat(matfile,data)
+
+# %% Fast Linear Assignment Problem using Auction Algorithm
+print('Matlab: Fast Linear Assignment Problem using Auction Algorithm')
+fastAuction="/home/gamorosino/local/fastAuction_v2.6"
+
+import subprocess
+
+cmd = """matlab -nodesktop -nosplash -r "addpath('"""+fastAuction+"""'); mat=load('"""+matfile+"""'); applyFastAuction(mat.cost) ; quit;"   """
+
+os.system(cmd + ' >> /tmp/output_py.txt')
+cmd="cat /tmp/output_py.txt"
+process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+output, error = process.communicate()
+print('output:', output.decode("utf-8"))
+
+
+#gnome-terminal -x bash -c ""	
+
+# %% Linear Sum Assigment  
+if lap_flag:
+    print('Linear Sum Assigment')
+    t0 = time()
+    row_ind1, col_ind1 = linear_sum_assignment(cost1)
+    print('%s sec' % (time() - t0))
+    total_cost1 = cost[row_ind1, col_ind1].sum()
+    print('lap cost=%s' % total_cost1)
 
 # %% MWFBM  
-
-print('MWFBM')
-t0 = time()
-row_ind, col_ind = min_weight_full_bipartite_matching(cost)
-print('%s sec' % (time() - t0))
-total_cost = cost[row_ind, col_ind].sum()
-print('mwfb cost=%s' % total_cost)
+if MWFBM_flag:
+    print('MWFBM')
+    t0 = time()
+    row_ind, col_ind = min_weight_full_bipartite_matching(cost)
+    print('%s sec' % (time() - t0))
+    total_cost = cost[row_ind, col_ind].sum()
+    print('mwfb cost=%s' % total_cost)
